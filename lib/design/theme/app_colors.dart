dc80597b0e1ba2beb7557184040c87c150a65312
Matsui01 <paulomatsui123@gmail.com>
2023-12-09 12:30:13 -0300

    <Feat>(Theme) Implementando tema

---
+++ lib/design/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  const AppColors._();
+  static const Color primary = Color(0xFFFFFFFF);
+}
---
+++ lib/design/theme/light_theme.dart
@@ -0,0 +1,18 @@
+import 'package:alarm/design/design.dart';
+import 'package:flutter/material.dart';
+
+ThemeData get light {
+  return ThemeData(
+    useMaterial3: true,
+    textTheme: const TextTheme(),
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: AppColors.primary,
+    ),
+    scaffoldBackgroundColor: AppColors.primary,
+    appBarTheme: const AppBarTheme(
+      surfaceTintColor: AppColors.primary,
+      backgroundColor: AppColors.primary,
+    ),
+  );
+}
